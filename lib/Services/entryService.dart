e179681c3b0b6fa6c7c6eff9a9a51e3431e16ba6
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-03-16 21:15:46 +0530

    Fixed issues in widget_test file

--- lib/Services/entryService.dart
+++ lib/Services/entryService.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'auth_service.dart';
-import 'db_services.dart';
 import 'dart:io';
 
 String email = getUserEmail();
