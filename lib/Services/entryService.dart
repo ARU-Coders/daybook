550c3e38376c413e09e683d811f079be18a3e6a4
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-03-19 20:38:24 +0530

    downloadUrl of image now gets stored in Firestore

--- lib/Services/entryService.dart
+++ lib/Services/entryService.dart
@@ -8,69 +8,92 @@ String email = getUserEmail();
 DocumentReference userDoc =
     FirebaseFirestore.instance.collection('users').doc(email);
 
-Future<String> uploadImage(Reference ref, File _image) async {
-  String url;
-  UploadTask uploadTask = ref.putFile(_image);
-  uploadTask.whenComplete(() async {
-    url = await ref.getDownloadURL();
-    print("Function wala url :" + url.toString());
-    return url;
-  });
-  return '';
-}
+// Future<String> uploadImage(Reference ref, File _image) async {
+//   String url;
+//   UploadTask uploadTask = ref.putFile(_image);
+//   uploadTask.whenComplete(() async {
+//     url = await ref.getDownloadURL();
+//     print("Function wala url :" + url.toString());
+//     return url;
+//   });
+//   return '';
+// }
 
 Future<List<String>> uploadFiles(List<String> _images) async {
-  // if()
-  print("upload files wala" + _images.toString());
+
   List<File> _imageFiles = _images.map((path) => File(path)).toList();
   List<String> imagesUrls = [];
   String url;
-  print("Starting the upload...\n");
-  print("imagefiles" + _imageFiles.toString());
+  // print("Starting the upload...\n");
+  // print("imagefiles" + _imageFiles.toString());
 
   String id = email;
   _imageFiles.forEach((_image) async {
     String imageRef = id + '/' + _image.path.split('/').last;
-    print(imageRef);
+    // print(imageRef);
     Reference ref = FirebaseStorage.instance.ref(imageRef);
-    print("reference" + ref.toString());
-    url = await uploadImage(ref, _image);
-    print("Returned url : " + url.toString());
-    imagesUrls.add(url);
+    // print("reference" + ref.toString());
 
-    //   UploadTask uploadTask = ref.putFile(_image);
-    //   uploadTask.whenComplete(() async{
-    //     String url = await ref.getDownloadURL();
+    // url = await uploadImage(ref, _image);
+    // print("Returned url : " + url.toString());
+    // imagesUrls.add(url);
+
+      UploadTask uploadTask = ref.putFile(_image);
+      // uploadTask.whenComplete(() async{
+      uploadTask.then((res) async{
+        // String url = await ref.getDownloadURL();
+        String url = await res.ref.getDownloadURL();
     //     print(url);
-    //     imagesUrls.add(url);
-    // }).catchError((onError) {
-    // print(onError);
-    // });
+        imagesUrls.add(url);
+    }).catchError((onError) {
+    print(onError);
+    });
   });
-
-  print("Upload finished ");
-  print(imagesUrls);
   return imagesUrls;
 }
 
 Future<DocumentReference> createEntry(
     String title, String content, String mood, List<String> images) async {
-  print(images);
-  List<String> imagesURLs;
-  imagesURLs = images.length > 0 ? await uploadFiles(images) : [];
-  print("uploadFiles() returned this  :");
-  print(imagesURLs);
+    var randomDoc = await userDoc.collection('entries').doc();
+    String docId  = randomDoc.id;
+
+  print("Ye rahe $images");
+  List<String> imagesURLs = [];
+  
+  if(images.length > 0){
+    String id = email;
+
+    await Future.wait(images.map((String _image) async {
+      String imageRef = id + '/' + _image.split('/').last;
+      Reference ref = FirebaseStorage.instance.ref(imageRef);
+      UploadTask uploadTask = ref.putFile(File(_image));
+
+
+      TaskSnapshot snapshot = await uploadTask.whenComplete(() async{
+        String downloadUrl = await ref.getDownloadURL();
+        imagesURLs.add(downloadUrl);
+      });
+
+    }), eagerError: true, cleanUp: (_) {
+      print('eager cleaned up');
+    });
+  }
+
+  else{
+    imagesURLs = [];
+  }
 
   DateTime now = new DateTime.now();
-  Future<DocumentReference> query = userDoc.collection('entries').add({
+  final task = await userDoc.collection('entries').doc(docId).set({
     'title': title,
     'content': content,
     'dateCreated': DateTime(now.year, now.month, now.day).toString(),
     'dateLastModified': DateTime(now.year, now.month, now.day).toString(),
     'mood': mood,
     'images': imagesURLs,
+    'docId': docId
   });
-  print(query);
+  DocumentReference query =  userDoc.collection('entries').doc(docId);
   return query;
 }
 
