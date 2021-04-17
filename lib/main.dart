ec213a0ea599eb790cbf67532afcccdcde13f9d6
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-04-17 08:23:40 +0530

    Add Speed Dial FAB in Entries Screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:daybook/Pages/edit_profile.dart';
 // import 'package:daybook/Pages/habit_stats.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'Pages/Entries/ocr_entry.dart';
 import 'Pages/profile.dart';
 import 'Pages/start.dart';
 import 'Pages/Authentication/signup.dart';
@@ -55,6 +56,7 @@ class MyApp extends StatelessWidget {
         '/login': (BuildContext context) => LoginPage(),
         '/home': (BuildContext context) => HomePage(),
         '/createEntry': (BuildContext context) => CreateEntryScreen(),
+        '/createOCREntry': (BuildContext context) => CreateOCREntryScreen(),
         '/createJourney': (BuildContext context) => CreateJourneyScreen(),
         '/createHabit': (BuildContext context) => CreateHabitScreen(),
         '/displayEntry': (BuildContext context) => DisplayEntryScreen(),
