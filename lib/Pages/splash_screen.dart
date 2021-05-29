1ff7d8c5141076efe67001876960858ef4657abd
Rajan Gaul <rajan.gaul@somaiya.edu>
2021-05-30 00:41:15 +0530

    Add Splash Screen

---
+++ lib/Pages/splash_screen.dart
@@ -0,0 +1,87 @@
+import "package:shared_preferences/shared_preferences.dart";
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:daybook/Utils/constantStrings.dart';
+import 'package:daybook/Utils/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  
+  startTime() async {
+    var _duration = Duration(seconds: 2);
+    return Timer(_duration, navigate);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  void navigate() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    bool _seen = (prefs.getBool(SHAREDPREFNAME) ?? false);
+    if(_seen){
+      /// If logged in redirect to home screen
+      if(FirebaseAuth.instance.currentUser != null){
+        print("Splash screen, user found!");
+        Navigator.popAndPushNamed(context, '/home');
+      }
+      /// Else redirect to Login screen
+      else{
+        Navigator.popAndPushNamed(context, '/login');
+      }
+    }
+    else{
+      await prefs.setBool(SHAREDPREFNAME, true);
+      // Show Intro Screens
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft, end: Alignment.topRight, colors: colorPalette,
+              ),
+            ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                child: AnimatedTextKit(
+                  animatedTexts:[
+                    TyperAnimatedText("Daybook", 
+                      textStyle: GoogleFonts.getFont("Allura", fontSize: 40, color: Colors.blueGrey[800], letterSpacing: 1.5
+                      ),
+                      speed: Duration(milliseconds: 100),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:daybook/Pages/Journeys/create_journey.dart';
 import 'package:daybook/Pages/Entries/display_entry.dart';
 import 'package:daybook/Pages/Journeys/display_journey.dart';
 import 'package:daybook/Pages/Journeys/select_entries.dart';
-import 'package:daybook/Pages/edit_profile.dart';
+import 'package:daybook/Pages/Profile/edit_profile.dart';
+import 'package:daybook/Pages/splash_screen.dart';
+import 'package:daybook/Provider/email_sign_in.dart';
+import 'package:daybook/Provider/google_sign_in.dart';
 import 'package:daybook/Provider/theme_change.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +15,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'Pages/Entries/pick_image_screen.dart';
 import 'Pages/Entries/ocr_entry.dart';
-import 'Pages/profile.dart';
-import 'Pages/start.dart';
+import 'Pages/Profile/profile.dart';
 import 'Pages/Authentication/signup.dart';
 import 'Pages/Authentication/login.dart';
 import 'Pages/home.dart';
@@ -52,27 +54,40 @@ class MaterialAppWithTheme extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeChanger>(context);
-    return MaterialApp(
-      theme: theme.getTheme,
-      debugShowCheckedModeBanner: false,
-      title: title,
-      home: StartPage(),
-      routes: <String, WidgetBuilder>{
-        '/start': (BuildContext context) => StartPage(),
-        '/signup': (BuildContext context) => Signup(),
-        '/login': (BuildContext context) => LoginPage(),
-        '/home': (BuildContext context) => HomePage(),
-        '/createEntry': (BuildContext context) => CreateEntryScreen(),
-        '/createOCREntry': (BuildContext context) => CreateOCREntryScreen(),
-        '/captureEntry': (BuildContext context) => PickImageScreen(),
-        '/createJourney': (BuildContext context) => CreateJourneyScreen(),
-        '/createHabit': (BuildContext context) => CreateHabitScreen(),
-        '/displayEntry': (BuildContext context) => DisplayEntryScreen(),
-        '/displayJourney': (BuildContext context) => DisplayJourneyScreen(),
-        '/selectEntries': (BuildContext context) => SelectEntriesScreen(),
-        '/profile': (BuildContext context) => ProfileScreen(),
-        '/editProfile': (BuildContext context) => EditProfile(),
-      },
-    );
+    return  
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<GoogleSignInProvider>(create: (context) {
+            return GoogleSignInProvider();
+            },
+          ),
+          ChangeNotifierProvider<EmailSignInProvider>(create: (context) {
+            return EmailSignInProvider();
+            },
+          ),
+        ],
+        child: MaterialApp(
+        theme: theme.getTheme,
+        debugShowCheckedModeBanner: false,
+        title: title,
+        home: SplashScreen(),
+        routes: <String, WidgetBuilder>{
+          '/splashScreen': (BuildContext context)=>SplashScreen(),
+          '/signup': (BuildContext context) => Signup(),
+          '/login': (BuildContext context) => LoginPage(),
+          '/home': (BuildContext context) => HomePage(),
+          '/createEntry': (BuildContext context) => CreateEntryScreen(),
+          '/createOCREntry': (BuildContext context) => CreateOCREntryScreen(),
+          '/captureEntry': (BuildContext context) => PickImageScreen(),
+          '/createJourney': (BuildContext context) => CreateJourneyScreen(),
+          '/createHabit': (BuildContext context) => CreateHabitScreen(),
+          '/displayEntry': (BuildContext context) => DisplayEntryScreen(),
+          '/displayJourney': (BuildContext context) => DisplayJourneyScreen(),
+          '/selectEntries': (BuildContext context) => SelectEntriesScreen(),
+          '/profile': (BuildContext context) => ProfileScreen(),
+          '/editProfile': (BuildContext context) => EditProfile(),
+        },
+        ),
+      );
   }
 }
